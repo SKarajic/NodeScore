c811c5359c6302afab6e406ca3f1368056b9ccb8
SKarajic <sandibosna@outlook.com>
2017-10-16 15:14:43 +0200

    reindentation

--- test/test.js
+++ test/test.js
@@ -11,150 +11,142 @@ let ns = new NodeScore(key);
 describe('Competition', () => {
     it('should get Competitions', (done) => {
         ns.competitions()
-        .then((comps) => {
-            expect(typeof JSON.parse(JSON.stringify(comps[0])).wrapper)
-                .to.equal('undefined');
-            expect(Array.isArray(comps)).to.equal(true);
-            expect(typeof comps[0].name).to.equal('string');
-            done();
-        })
-        .catch((err) => console.log(err));
+            .then((comps) => {
+                expect(typeof JSON.parse(JSON.stringify(comps[0])).wrapper)
+                    .to.equal('undefined');
+                expect(Array.isArray(comps)).to.equal(true);
+                expect(typeof comps[0].name).to.equal('string');
+                done();
+            })
+            .catch((err) => console.log(err));
     });
-
     it('should get Competition with id 1322', (done) => {
         ns.competition(1322)
-        .then((comp) => {
-            expect(parseInt(comp.id)).to.equal(1322);
-            done();
-        })
-        .catch((err) => console.log(err));
+            .then((comp) => {
+                expect(parseInt(comp.id)).to.equal(1322);
+                done();
+            })
+            .catch((err) => console.log(err));
     });
     it('should should fail getting Competition with id 1', (done) => {
         ns.competition(1)
-        .catch((err) => {
-            expect(err).to.be.an.instanceOf(Error);
-            done();
-        });
+            .catch((err) => {
+                expect(err).to.be.an.instanceOf(Error);
+                done();
+            });
     });
     it('should get Competition from Standings', (done) => {
         ns.standings(1322)
-        .then((stands) => stands[0].competition())
-        .then((comp) => {
-            expect(comp.id).to.equal(1322);
-            done();
-        })
-        .catch((err) => console.log(err));
+            .then((stands) => stands[0].competition())
+            .then((comp) => {
+                expect(comp.id).to.equal(1322);
+                done();
+            })
+            .catch((err) => console.log(err));
     });
 });
 
 describe('Standings', () => {
     it('should get Standings', (done) => {
         ns.standings(1322)
-        .then((stands) => {
-            expect(typeof JSON.parse(JSON.stringify(stands[0])).wrapper)
-                .to.equal('undefined');
-            expect(Array.isArray(stands)).to.equal(true);
-            expect(typeof stands[0].season).to.equal('string');
-            done();
-        })
-        .catch((err) => console.log(err));
+            .then((stands) => {
+                expect(typeof JSON.parse(JSON.stringify(stands[0])).wrapper)
+                    .to.equal('undefined');
+                expect(Array.isArray(stands)).to.equal(true);
+                expect(typeof stands[0].season).to.equal('string');
+                done();
+            })
+            .catch((err) => console.log(err));
     });
-
     it('should get Standings from Competition', (done) => {
         ns.competition(1322)
-        .then((comp) => comp.standings())
-        .then((stands) => {
-            const stand = stands[0];
-            expect(Array.isArray(stands)).to.equal(true);
-            expect(stand.compId).to.equal(1322);
-            expect(typeof stand.wins()).to.equal('number');
-            expect(typeof stand.draws()).to.equal('number');
-            expect(typeof stand.losses()).to.equal('number');
-            expect(typeof stand.gamesPlayed()).to.equal('number');
-            expect(typeof stand.goalsScored()).to.equal('number');
-            expect(typeof stand.goalsAccepted()).to.equal('number');
-            expect(typeof stand.goalDifference()).to.equal('number');
-            expect(typeof stand.home.gamesPlayed).to.equal('number');
-            expect(typeof stand.away.gamesPlayed).to.equal('number');
-            done();
-        })
-        .catch((err) => console.log(err));
+            .then((comp) => comp.standings())
+            .then((stands) => {
+                const stand = stands[0];
+                expect(Array.isArray(stands)).to.equal(true);
+                expect(stand.compId).to.equal(1322);
+                expect(typeof stand.wins()).to.equal('number');
+                expect(typeof stand.draws()).to.equal('number');
+                expect(typeof stand.losses()).to.equal('number');
+                expect(typeof stand.gamesPlayed()).to.equal('number');
+                expect(typeof stand.goalsScored()).to.equal('number');
+                expect(typeof stand.goalsAccepted()).to.equal('number');
+                expect(typeof stand.goalDifference()).to.equal('number');
+                expect(typeof stand.home.gamesPlayed).to.equal('number');
+                expect(typeof stand.away.gamesPlayed).to.equal('number');
+                done();
+            })
+            .catch((err) => console.log(err));
     });
-
-    it('should should fail getting Standings from Competition with id 1',
-    (done) => {
+    it('should should fail getting Standings from Competition', (done) => {
         ns.standings(1)
-        .catch((err) => {
-            expect(err).to.be.an.instanceOf(Error);
-            done();
-        });
+            .catch((err) => {
+                expect(err).to.be.an.instanceOf(Error);
+                done();
+            });
     });
 });
 
 describe('Teams', () => {
     it('should get Team with id 9002', (done) => {
         ns.team(9002)
-        .then((team) => {
-            expect(team.id).to.equal(9002);
-            expect(typeof JSON.parse(JSON.stringify(team)).wrapper)
-            .to.equal('undefined');
-            expect(typeof team.statistics.wins()).to.equal('number');
-            expect(typeof team.statistics.draws()).to.equal('number');
-            expect(typeof team.statistics.losses()).to.equal('number');
-            expect(typeof team.statistics.goals()).to.equal('object');
-            expect(typeof team.statistics.home.wins).to.equal('number');
-            done();
-        })
-        .catch((err) => console.log(err));
+            .then((team) => {
+                expect(team.id).to.equal(9002);
+                expect(typeof JSON.parse(JSON.stringify(team)).wrapper)
+                .to.equal('undefined');
+                expect(typeof team.statistics.wins()).to.equal('number');
+                expect(typeof team.statistics.draws()).to.equal('number');
+                expect(typeof team.statistics.losses()).to.equal('number');
+                expect(typeof team.statistics.goals()).to.equal('object');
+                expect(typeof team.statistics.home.wins).to.equal('number');
+                done();
+            })
+            .catch((err) => console.log(err));
     });
-
     it('should get Team from Standing', (done) => {
         ns.standings(1322)
-        .then((stands) => stands[0].team())
-        .then((team) => {
-            expect(typeof team.id).to.equal('number');
-            done();
-        })
-        .catch((err) => console.log(err));
+            .then((stands) => stands[0].team())
+            .then((team) => {
+                expect(typeof team.id).to.equal('number');
+                done();
+            })
+            .catch((err) => console.log(err));
     });
-
     it('should should fail getting Team with no id', (done) => {
         ns.team()
-        .catch((err) => {
-            expect(err).to.be.an.instanceOf(Error);
-            done();
-        });
+            .catch((err) => {
+                expect(err).to.be.an.instanceOf(Error);
+                done();
+            });
     });
 });
 
 describe('Player', () => {
     it('should get Player with id 1', (done) => {
         ns.player(1)
-        .then((player) => {
-            expect(player.id).to.equal(1);
-            done();
-        })
-        .catch((err) => console.log(err));
+            .then((player) => {
+                expect(player.id).to.equal(1);
+                done();
+            })
+            .catch((err) => console.log(err));
     });
-
     it('should get Player from Team through Squad', (done) => {
         ns.team(9002)
-        .then((team) => team.squad.members[0].player())
-        .then((player) => {
-            expect(typeof player.id).to.equal('number');
-            done();
-        })
-        .catch((err) => console.log(err));
+            .then((team) => team.squad.members[0].player())
+            .then((player) => {
+                expect(typeof player.id).to.equal('number');
+                done();
+            })
+            .catch((err) => console.log(err));
     });
-
     it('should get Player from Team through TransferIn', (done) => {
         ns.team(9002)
-        .then((team) => team.transfers.in[0].player())
-        .then((player) => {
-            expect(typeof player.id).to.equal('number');
-            done();
-        })
-        .catch((err) => console.log(err));
+            .then((team) => team.transfers.in[0].player())
+            .then((player) => {
+                expect(typeof player.id).to.equal('number');
+                done();
+            })
+            .catch((err) => console.log(err));
     });
 });
 
@@ -166,7 +158,6 @@ describe('Utilities', () => {
                 done();
             });
     });
-
     it('should fail getting something', (done) => {
         getJSON('http://alinkwithnoendpoint.kr/')
             .catch((err) => {
